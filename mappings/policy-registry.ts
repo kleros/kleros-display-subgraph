4533b8eac4300a778743fa3b1fc53dec9c0946d3
Harman-singh-waraich <Waraichharman68@gmail.com>
2024-04-22 18:05:40 +0700

    refactor: lint-policy-registry-mapping

--- mappings/policy-registry.ts
+++ mappings/policy-registry.ts
@@ -1,40 +1,38 @@
-import { PolicyUpdate as PolicyUpdateEvent } from "../generated/PolicyRegistry/PolicyRegistry"
-import { Court,CourtMetadata } from "../generated/schema"
-import { json, Bytes, dataSource, log } from "@graphprotocol/graph-ts";
+import { PolicyUpdate as PolicyUpdateEvent } from "../generated/PolicyRegistry/PolicyRegistry";
+import { Court, CourtMetadata } from "../generated/schema";
+import { json, Bytes, dataSource } from "@graphprotocol/graph-ts";
 import { CourtMetadata as CourtMetadataTemplate } from "../generated/templates";
 
 export function handlePolicyUpdate(event: PolicyUpdateEvent): void {
-  let court = Court.load(
-    event.params._subcourtID.toString()
-  )
+  let court = Court.load(event.params._subcourtID.toString());
 
   if (!court) {
-    court = new Court(
-      event.params._subcourtID.toString()
-    )
+    court = new Court(event.params._subcourtID.toString());
   }
-  const ipfsHash =  event.params._policy.replace('/ipfs/', '')
+
+  const ipfsHash = event.params._policy.replace("/ipfs/", "");
   court.policy = ipfsHash;
   court.metadata = ipfsHash;
 
-  CourtMetadataTemplate.create(ipfsHash)
+  CourtMetadataTemplate.create(ipfsHash);
   court.save();
-
 }
 
 export function handleCourtMetadata(content: Bytes): void {
-  let courtMetadata = new CourtMetadata(dataSource.stringParam())
-  const value = json.fromBytes(content).toObject()
-  log.debug("got value {}",[json.fromBytes(content).toString()])
+  let courtMetadata = new CourtMetadata(dataSource.stringParam());
+  const value = json.fromBytes(content).toObject();
+
   if (value) {
-    const name = value.get('name');
-    courtMetadata.name = name? name.toString() : null;
-  
-    const description = value.get('description');
-    courtMetadata.description = description? description.toString() : null;
-  
-    const requiredSkills = value.get('requiredSkills');
-    courtMetadata.requiredSkills = requiredSkills? requiredSkills.toString() : null;
-    courtMetadata.save()
+    const name = value.get("name");
+    courtMetadata.name = name ? name.toString() : null;
+
+    const description = value.get("description");
+    courtMetadata.description = description ? description.toString() : null;
+
+    const requiredSkills = value.get("requiredSkills");
+    courtMetadata.requiredSkills = requiredSkills
+      ? requiredSkills.toString()
+      : null;
+    courtMetadata.save();
   }
 }
